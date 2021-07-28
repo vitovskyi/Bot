34227bc74cd9156bcfa0dbbfecf28a476cc1d13c
Roman Vitovskyi <r.vitovskij@gmail.com>
2021-07-28 11:43:18 -0400

    Add new files

---
+++ bot.py
@@ -0,0 +1,31 @@
+import logging
+import settings
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+
+logging.basicConfig(format='%(asctime)s -%(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+def start_bot(update: Updater, context: CallbackContext):
+    mytext = """
+    Hi {}
+    Next time""".format(update.message.chat.first_name)
+    update.message.reply_text(mytext)
+
+def chat(update: Updater, context: CallbackContext):
+    text =  update.message.text
+    update.message.reply_text(text)
+
+def main():
+    updtr = Updater(settings.TOKEN_TELEGRAM) 
+    
+    updtr.dispatcher.add_handler(CommandHandler("start", start_bot))
+    updtr.dispatcher.add_handler(MessageHandler(Filters.text,chat))
+
+
+    updtr.start_polling()
+    updtr.idle()
+if __name__=="__main__":
+    logging.info('Bot started!')
+    main()
